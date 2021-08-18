56d7797a43e1be5ec22fdbc4b8e51dd74ba62e89
Catalpam <893816760@qq.com>
2021-08-18 17:37:00 +0800

    init develop files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Image Uploader  自定义图床
+
+本脚本软件用于Typora的图像上传，可通过Markdown文件的标题自动重命名文件并将其上传。
---
+++ uploader.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// 获取命令行参数
+	var args = os.Args
+	fmt.Println(args)
+
+	return
+}
